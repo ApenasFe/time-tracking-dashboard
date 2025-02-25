394399a99642bd126eb65143de6ed9f9b594c300
ApenasFe <fe.my.projects@gmail.com>
2025-02-25 16:09:35 -0300

    Alteração em como os dados JSON são tratados com click do botão.
    Implementação de alguns breakpoints e ajustes para manter responsividade.

--- index.js
+++ index.js
@@ -1,53 +1,69 @@
-async function readJson(callback, timeKey){
-    //Fazendo fetch e parse do arquivo data.json
-    let dataObject = await fetch('./data.json');
-    let dataText = await dataObject.text();
-    const data = JSON.parse(dataText);
-    let choosenKey = timeKey
-    callback(data, choosenKey);
+async function readJson(displayCallback, timeKey){
+  //Fazendo fetch e parse do arquivo data.json
+  let dataObject = await fetch('./data.json');
+  let dataText = await dataObject.text();
+  const data = JSON.parse(dataText);
+  let choosenKey = timeKey;
+  displayCallback(data, choosenKey);
 };
 
 //Exibe os dados JSON
 function displayData(myData, selectedTimeKey){
-    for (let i = 0; i < myData.length; i++){
-        let activityTitle = myData[i].title;
-        let activityTimeframes = myData[i].timeframes;
-        let timeframeKey = selectedTimeKey;
-        //Cria um container de atividade com os dados JSON
-        createContainer(activityTitle, activityTimeframes, timeframeKey);
-        };
+  for (let i = 0; i < myData.length; i++){
+      let activityTitle = myData[i].title;
+      let activityTimeframes = myData[i].timeframes;
+      let timeframeKey = selectedTimeKey;
+      //Cria um container de atividade com os dados JSON
+      createContainer(activityTitle, activityTimeframes, timeframeKey);
+      };
 };
 
 //Função da criação dos containers com os dados JSON
 function createContainer(activity, timeFrame, time) {
-    document.getElementById('activity-container-id').innerHTML += 
+  document.getElementById('activity-container-id').innerHTML += 
 
-    `<div class="banner-box" id="${activity.toLowerCase().replace(/ /, "-")}-banner">
+  `<div class="activity-banner-box">
+    <div class="banner-box" id="${activity.toLowerCase().replace(/ /, "-")}-banner">
       <img src="images/icon-${activity.toLowerCase().replace(/ /, "-")}.svg" alt="Activity Icon" class="activity-icon">
     </div>
     
     <div class="activity-box" id="${activity.toLowerCase().replace(/ /, "-")}-info">
       <div class="activity-info" id="current-time">
         <p class="activity-text" id="${activity.toLowerCase().replace(/ /, "-")}-text">${activity}</p>
-        <p class="activity-text" id="current-time-text">${timeFrame[time].current}hrs</p>
+        <p class="activity-text" id="current-time-text" name="${activity.toLowerCase().replace(/ /, "-")}-current-time">${timeFrame[time].current}hrs</p>
       </div>
       
       <div class="activity-info" id="last-time">
         <div class="ellipsis-grid">
           <img src="images/icon-ellipsis.svg" class="ellipsis-icon">
         </div>
-        <p class="activity-text" id="last-time-text">Last Week - ${timeFrame[time].previous}hrs</p>
+        <p class="activity-text" id="last-time-text" name="${activity.toLowerCase().replace(/ /, "-")}-previous-time">Last Week - ${timeFrame[time].previous}hrs</p>
       </div>
-    </div>`;
+    </div>
+  </div>`;
 };
 
-function clearContainer(btnKey) {
-    document.getElementById('activity-container-id').innerHTML = ''
-    readJson(displayData, btnKey);
+//Função dos botões para mudar as horas no rastreador de acordo com os dados JSON
+async function dataSwap(btnKey) {
+let jsonObject = await fetch('./data.json');
+let jsonDataText = await jsonObject.text();
+const jsonData = JSON.parse(jsonDataText);
+
+for (i = 0; i < jsonData.length ;i++) {
+  let activityTitle = jsonData[i].title.toLowerCase().replace(/ /, '-');
+  let activityTime = jsonData[i].timeframes[btnKey];
+  currentElement = activityTitle + '-current-time';
+  previousElement = activityTitle + '-previous-time';
+  
+  document.getElementsByName(currentElement)[0].innerHTML = activityTime['current'] + 'hrs';
+  document.getElementsByName(previousElement)[0].innerHTML = 'Last Week - ' + activityTime['previous'] + 'hrs';
 }
+
+};
 
 readJson(displayData, 'weekly');
 
-document.getElementById('daily-btn').addEventListener('click', function(){clearContainer('daily');});
-document.getElementById('weekly-btn').addEventListener('click', function(){clearContainer('weekly');});
-document.getElementById('monthly-btn').addEventListener('click', function(){clearContainer('monthly');});
+//Botões
+document.getElementById('daily-btn').addEventListener('click', function(){dataSwap('daily');});
+document.getElementById('weekly-btn').addEventListener('click', function(){dataSwap('weekly');});
+document.getElementById('monthly-btn').addEventListener('click', function(){dataSwap('monthly');});
